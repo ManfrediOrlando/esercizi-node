13699e545e9ac43a659546d5e0da919d4bfea4fa
Manfredi <manfrediorlando@yahoo.it>
2022-08-19 11:29:51 +0200

    es4

---
+++ dist/es4.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+require("express-async-errors");
+const app = (0, express_1.express)();
+app.get("/", (request, response) => {
+    response.send("up and running!");
+});
+const port = 3000;
+app.listen(port, () => {
+    console.log(`[server]: server is running at http://localhost:${port}`);
+});
+//# sourceMappingURL=es4.js.map
