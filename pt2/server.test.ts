6e45572bce7c5ae8139be11c8a3aa2dbeafaf5de
Manfredi <manfrediorlando@yahoo.it>
2022-08-29 18:06:19 +0200

    added post/planets route (es 7)

--- pt2/server.test.ts
+++ pt2/server.test.ts
@@ -7,33 +7,17 @@ import app from "./app";
 const request = supertest(app);
 
 test("GET /planets", async () => {
-    const planets = [
-        {
-            id: 3,
-            name: "Mercury",
-            description: null,
-            diameter: 1234,
-            moons: 0,
-            createdAt: "2022-08-29T14:30:30.704Z",
-            updatedAt: "2022-08-29T14:30:15.773Z",
-        },
-        {
-            id: 4,
-            name: "Venus",
-            description: null,
-            diameter: 5678,
-            moons: 0,
-            createdAt: "2022-08-29T14:31:25.882Z",
-            updatedAt: "2022-08-29T14:31:13.530Z",
-        },
-    ];
-    //@ts-ignore
-    prismaMock.planet.findMany.mockResolvedValue(planets);
+    const planet = {
+        name: "Mercury",
+        diameter: 1234,
+        moons: 0,
+    };
 
     const response = await request
-        .get("/planets")
-        .expect(200)
+        .post("/planets")
+        .send(planet)
+        .expect(201)
         .expect("content-type", /application\/json/);
 
-    expect(response.body).toEqual(planets);
+    expect(response.body).toEqual(planet);
 });
