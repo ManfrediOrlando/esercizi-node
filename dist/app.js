bcfeb86452ed9a35a55faeec0aacd1411de28fa4
Manfredi <manfrediorlando@yahoo.it>
2022-08-29 09:49:20 +0200

    prisma

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
-app.get("/planets", (request, response) => {
-    response.json([
-        { name: "Mercury" },
-        { name: "Venus" },
-        { name: "Jupiter" },
-    ]);
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
+    response.json(planets);
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
