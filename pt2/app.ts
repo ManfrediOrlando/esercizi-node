7b777dc33aae0d2ddb70acb1d905ebc3ae9e6e8a
Manfredi <manfrediorlando@yahoo.it>
2022-08-30 17:21:50 +0200

    es 8 and 9

--- pt2/app.ts
+++ pt2/app.ts
@@ -48,6 +48,42 @@ app.post(
     }
 );
 
+app.put(
+    "/planets/:id(\\d+)",
+    validate({ body: planetSchema }),
+    async (request, response, next) => {
+        const planetId = Number(request.params.id);
+        const planetData: PlanetData = request.body;
+
+        try {
+            const planet = await prisma.planet.update({
+                where: { id: planetId },
+                data: planetData,
+            });
+
+            response.status(200).json(planet);
+        } catch (error) {
+            response.status(404);
+            next(`cannot PUT /planets/${planetId}`);
+        }
+    }
+);
+
+app.delete("/planets/:id(\\d+)", async (request, response, next) => {
+    const planetId = Number(request.params.id);
+
+    try {
+        await prisma.planet.delete({
+            where: { id: planetId },
+        });
+
+        response.status(204).end();
+    } catch (error) {
+        response.status(404);
+        next(`cannot DELETE /planets/${planetId}`);
+    }
+});
+
 app.use(ValidationErrorMiddleware);
 
 export default app;
