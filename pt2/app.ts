e6b4d578d80760d2651bdc5d2ecc9c0c19ff9197
Manfredi <manfrediorlando@yahoo.it>
2022-08-31 09:54:31 +0200

    es 11 upload file and add route

--- pt2/app.ts
+++ pt2/app.ts
@@ -11,6 +11,10 @@ import {
     PlanetData,
 } from "./lib/validation";
 
+import { initMulterMiddleware } from "./lib/middleware/multer";
+
+const upload = initMulterMiddleware();
+
 const corsOptions = {
     origin: "http://localhost:8080",
 };
@@ -91,6 +95,22 @@ app.delete("/planets/:id(\\d+)", async (request, response, next) => {
     }
 });
 
+app.post(
+    "/planets/:id(\\d+)/photo",
+    upload.single("photo"),
+    async (request, response, next) => {
+        console.log("request.file", request.file);
+
+        if (!request.file) {
+            response.status(400);
+            return next("no photo file uploaded.");
+        }
+
+        const photoFilename = request.file.filename;
+
+        response.status(201).json({ photoFilename });
+    }
+);
 app.use(ValidationErrorMiddleware);
 
 export default app;
